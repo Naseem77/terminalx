586b9bf481cecda3da3f5c94d88ca85506115975
milad <miladbannourah@outlook.com>
2024-01-11 16:16:09 +0200

    adding the xpaths for the locators

--- logic/LoginPage.ts
+++ logic/LoginPage.ts
@@ -11,6 +11,10 @@ export class LoginPage extends BasePage{
 
     constructor(page: Page){
         super(page);
+        this.connectBtn = page.locator('//*[@id="app-root"]/div[2]/header/div/div[2]/div[1]/div[1]/div/div/div/a');
+        this.emailFeild = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/div[1]/div/input');
+        this.passwordFeild = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/div[2]/div/input');
+        this.loginBtn = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/button[3]');
         this.initPage();
     }
    
