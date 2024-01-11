a75460fabb01f0b460563a1a31bbebf2f6aca2e8
milad <miladbannourah@outlook.com>
2024-01-11 16:44:07 +0200

    finished all the functions and tested how they work in a demo.spec.ts

--- logic/LoginPage.ts
+++ logic/LoginPage.ts
@@ -1,6 +1,6 @@
-import { Locator,  Page } from '@playwright/test';
+import { Locator,  Page, expect } from '@playwright/test';
 import { BasePage } from '../infra/BasePage';
-import {url, username, password} from "../config.json";
+import {username, password} from "../config.json";
 
 
 export class LoginPage extends BasePage{
@@ -9,6 +9,7 @@ export class LoginPage extends BasePage{
     private emailFeild: Locator;
     private passwordFeild: Locator;
     private loginBtn: Locator;
+    private nameLabel: Locator;
 
     constructor(page: Page){
         super(page);
@@ -16,6 +17,7 @@ export class LoginPage extends BasePage{
         this.emailFeild = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/div[1]/div/input');
         this.passwordFeild = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/div[2]/div/input');
         this.loginBtn = page.locator('//*[@id="top-portal-root"]/div[3]/div[1]/form/button[3]');
+        this.nameLabel = page.locator('//*[@id="app-root"]/div[2]/header/div/div[2]/div[1]/div[1]/div/div/div/button/span[2]');
         this.initPage();
     }
 
@@ -42,6 +44,10 @@ export class LoginPage extends BasePage{
         await this.fillEmail();
         await this.fillPassword();
         await this.clickLogin();
+    }
+
+    returUserName = async (): Promise<String> => {
+        return await this.nameLabel.innerText();
     }
    
 }
