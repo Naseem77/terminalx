6c671eba2d55dee8e60a1074321766cc3f4f0e65
salehzarora <salehzarora.z.007@gmail.com>
2024-01-14 00:42:00 +0200

    change add item to cart test

--- tests/demo2.spec.ts
+++ tests/demo2.spec.ts
@@ -2,12 +2,18 @@ import { test, expect } from '@playwright/test';
 import { chromium, Browser, Page } from 'playwright';
 import { LoginPage } from '../logic/LoginPage';
 import { MainPage } from '../logic/MainPage';
+import { BrowserWrapper } from '../infra/browserWrapper';
+import configJson from "../config.json"
 
-test("add item to cart", async ({page}) => {
-    await page.goto("https://www.terminalx.com/");
+test("add item to cart", async ({ page }) => {
+    const browser = new BrowserWrapper;
+    page = await browser.getPage(configJson.url);
 
     const mainPage = new MainPage(page);
     await mainPage.addItemToCart();
     const count = await mainPage.getItemCountInCart();
     expect(count).toBe('1');
-}) 
+})
+
+
+
