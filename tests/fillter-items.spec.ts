17b55187d142eda83681327bee6d3eefa676baa6
salehzarora <salehzarora.z.007@gmail.com>
2024-01-14 14:08:29 +0200

    creating filter items test

---
+++ tests/fillter-items.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import { chromium, Browser, Page } from 'playwright';
+import { LoginPage } from '../logic/LoginPage';
+import { MainPage } from '../logic/MainPage';
+import { BrowserWrapper } from '../infra/browserWrapper';
+import configJson from "../config.json"
+import { describe } from 'node:test';
+
+test.describe('Filter Items Validation', () => {
+
+
+
+    test("filter items shirts page ", async ({ page }) => {
+
+    })
+
+})
+
+
+
