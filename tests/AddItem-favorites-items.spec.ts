b155bd012bab0fc32c16b864c81663eb0c631043
Naseem Ali <34807727+Naseem77@users.noreply.github.com>
2024-01-15 14:45:24 +0200

    Remove item my fav fixes

--- tests/AddItem-favorites-items.spec.ts
+++ tests/AddItem-favorites-items.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { BrowserWrapper } from '../infra/ui/browserWrapper'
-import { addItem2Fav, removeItem2Fav} from '../config/items.json'
-import { setRemoveItemToFavoritestRequest } from '../logic/api/request-body/removeItemFromFavorites';
+import { addItem2Fav } from '../config/items.json'
 import { setAddItemToFavoritestRequest } from '../logic/api/request-body/addItemToFavoritesRequest'
 import urls from '../config/urls.json'
 import { FavoritesListPage } from '../logic/POM/favoritesListPage';
@@ -33,17 +32,5 @@ test.describe('Cart Items Tests', () => {
     expect(await favoritesListPage.getItemsCount()).toBe(response.data.addProductsToWishlist.anyWishlist.items_count)
 
   });
-
-  test('Add Item to Favorites via Ui -> remove Item from Favorites via API -> validate item removed via ui', async () => {
-    favoritesListPage = await browser.createNewPage(FavoritesListPage)
-    await browser.navigateTo(urls.ui.wishListUrl)
-    const addItemData = setRemoveItemToFavoritestRequest(removeItem2Fav);
-    
-    const response = await apiCall.RemoveItemFromFavorites(addItemData);
-    await favoritesListPage.refreshPage()
-    expect(0).toBe(response?.data.removeProductsFromAnyWishlistById.anyWishlist.items_count)
-
-  });
-
 
 });
