6cfa4c72362e11dafb4d73c09ede04e434260fe4
salehzarora <salehzarora.z.007@gmail.com>
2024-01-15 14:31:02 +0200

    fix SearchBar test+new POM

--- tests/search-bar.spec.ts
+++ tests/search-bar.spec.ts
@@ -1,51 +1,29 @@
 import { test, expect } from '@playwright/test';
-import { BrowserWrapper } from '../infra/browserWrapper';
-import configJson from "../config.json"
-import { SearchItems } from '../logic/SearchBar';
-
-
-
-
-
+import { SearchItems } from '../logic/POM/SearchBar';
+import { BrowserWrapper } from '../infra/ui/browserWrapper';
+import config from "../config/urls.json";
 
 test.describe('Search For Items In SearchBar', () => {
     let browser: BrowserWrapper
-    let page;
-
-
-    test.beforeAll(async () => {
-        browser = new BrowserWrapper;
-
-    });
-
+    let search: SearchItems;
     test.beforeEach(async () => {
-
-        page = await browser.getPage(configJson.url);
+        browser = new BrowserWrapper();
     });
-
-
-
-
-
+    test.afterEach(async () => {
+        await browser.closeBrowser();
+    });
     const searchData = [
         { searchItem: 'טי שירט ריצה Nike Miler Flash', expected: 'טי שירט ריצה Nike Miler Flash' },
         { searchItem: 'סניקרס עור BB550VGA / גברים', expected: 'סניקרס עור BB550VGA / גברים' },
         { searchItem: 'חולצת טטרה עם כפתורים / 5Y-14Y', expected: 'חולצת טטרה עם כפתורים / 5Y-14Y' }
-
     ];
     searchData.forEach(({ searchItem, expected }) => {
         test(`search for ${searchItem} -> validate results as the ${expected} `, async () => {
-            const search = new SearchItems(page);
+            search = await browser.createNewPage(SearchItems)
+            await browser.navigateTo(config.ui.url)
             await search.fillSearchInput(searchItem, searchItem);
-            expect(search.checkForSearch(expected)).toBeTruthy();
-
-
-            await page.waitForTimeout(5000);
-
+            expect(search.checkForSearch(expected)).toBeTruthy()
 
         });
     })
-
-
-
 })
