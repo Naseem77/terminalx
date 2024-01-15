d99bd84146f5c153f3560abba0a6bed60f00f7ea
milad <miladbannourah@outlook.com>
2024-01-15 12:32:09 +0200

    edited navBar.ts to use waitfoeTimeout from utils.ts

--- logic/POM/navBar.ts
+++ logic/POM/navBar.ts
@@ -1,5 +1,6 @@
 import { Locator, Page, expect } from '@playwright/test';
 import { BasePage } from '../../infra/ui/BasePage';
+import { waitForTimeOut } from '../../infra/utils';
 
 
 export class NavBar extends BasePage {
@@ -35,7 +36,7 @@ export class NavBar extends BasePage {
 
     removeItem = async () => {
         await this.cartBtn.click();
-        await this.page.waitForTimeout(500);//change for all to be in utils
+        await waitForTimeOut(this.page)
         const removeBtn = this.page.locator("//div[@data-tip='הסר פריט']/button[1]");
         await removeBtn.click();
         await this.page.waitForTimeout(500);
