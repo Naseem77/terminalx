3f28212b21aa1d0718b5c1116a7f1c9ddc5e9c40
milad <miladbannourah@outlook.com>
2024-01-15 13:29:08 +0200

    added the locators and the constructor of addressPage.ts

--- logic/POM/addressPage.ts
+++ logic/POM/addressPage.ts
@@ -1,12 +1,20 @@
 import { Locator, Page } from '@playwright/test';
 import { NavBar } from './navBar';
-import { waitForElementToBeVisible } from '../../infra/utils';
 
 export class addressPage extends NavBar {
+
+    private firstNameFeild: Locator;
+    private lastNameFeild: Locator;
+    private cityFeild: Locator;
+    private phoneNumFeild: Locator;
 
     constructor(page: Page) {
         super(page);
         this.initPage();
+        this.firstNameFeild = page.locator("//input[@name='firstname']");
+        this.lastNameFeild = page.locator("//input[@name='lastname']");
+        this.cityFeild = page.locator("//input[@id='downshift-0-input']");
+        this.phoneNumFeild = page.locator("//input[@name='telephone']");
     }
 
     
