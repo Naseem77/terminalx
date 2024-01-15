3f79d06bf0dc7b42fef23193e39113af8704902c
milad <miladbannourah@outlook.com>
2024-01-15 14:06:16 +0200

    added a the rest of the data in address,json

--- tests/address-page.spec.ts
+++ tests/address-page.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { BrowserWrapper } from '../infra/ui/browserWrapper'
 import urls from '../config/urls.json'
 import { addressPage } from '../logic/POM/addressPage';
-import {first_name, last_name, city, phone} from "../config/addressInfo.json";
+import {first_name, last_name, city, street, house_num, mikood, phone} from "../config/addressInfo.json";
 
 test.describe('Cart Items Tests', () => {
   let browser: BrowserWrapper;
